6482e8d356fb1fa71198b9b62b705f19c477078d
Achalpvyas <achalpvyas@gmail.com>
2019-09-29 17:08:18 -0400

    updated main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
-#include <lib.hpp>
+/**
+ *  \brief    File used to input setpoint and actual velocity
+ *  \file     main file
+ *  \author   part1: Vamshi Kumar Bogoju(driver) Achal Pragneshkumar Vyas(navigator)
+ *  \author   part2: Achal Pragneshkumar Vyas(driver) Vamshi Kumar Bogoju(navigator)
+ *  \copyright  Vamshi Kumar Bogoju and Achal Pragneshkumar Vyas
+ */
 
-int main()
-{
-    dummy();
+#include <iostream>
+#include "../include/PIdControl.h"
+
+int main() {
+    double setPoint, actualVelocity;
+    std::cout << "Enter the Setpoint " << std::endl;
+    std::cin >> setPoint;
+    std::cout << "Enter the actual velocity " << std::endl;
+    std::cin >> actualVelocity;
+
+
+    PIdControl pid;
+    double newVel = pid.compute(setPoint, actualVelocity);
+    std::cout << "The new velocity is " << newVel << std::endl;
     return 0;
 }
+
