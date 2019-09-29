08e29bab4999dbdc7430a39919ab9d038d03bc97
vamshibogoju <vamshikumar0401@gmail.com>
2019-09-29 19:24:30 -0400

    updated the main file in the test folder with comments

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,8 @@
 /**
- *  \brief    File used for testing
- *  \file     main.cpp
- *  \author   part1: Vamshi Kumar Bogoju(driver) Achal Pragneshkumar Vyas(navigator)
- *  \copyright  Vamshi Kumar Bogoju and Achal Pragneshkumar Vyas
+ *  @brief    File used for testing
+ *  @file     main.cpp
+ *  @author   part1: Vamshi Kumar Bogoju(driver) Achal Pragneshkumar Vyas(navigator)
+ *  @copyright  Vamshi Kumar Bogoju and Achal Pragneshkumar Vyas
  */
 
 #include <gtest/gtest.h>
